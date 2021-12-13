91ece3327596c5c9379df0dcea270213accfc0f4
TeeToo <91502673+TeeToo136999@users.noreply.github.com>
2021-12-13 10:30:10 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,38 +6,41 @@ import time
 def main():
 
     mozne_akce = ["Kámen", "Nužky", "Papír"]
+    vyber = ["Kámen", "kámen", "Kamen", "kamen", "Papír", "papír",
+             "Papir", "papir", "Nužky", "nužky", "Nuzky", "nuzky"]
     Ty = 0
     Pc = 0
 
     while Ty < 5 and Pc < 5:
         user_action = input("Zadej Kamen/Nužky/Papír: ")
         computer_action = random.choice(mozne_akce)
-        print(
-            f"\nTvoje volba {user_action}, Volba počítače {computer_action}.\n")
-
-        if user_action == computer_action:
-            print(f"Oba jste vybrali stejný znak {user_action}. Je to remíza!")
-        elif user_action == "Kámen" or "kámen" or "Kamen" or "kamen":
-            if computer_action == "Nužky":
-                print("Kámen tupí nužky! Vyhrál jsi.")
-                Ty += 1
-            else:
-                print("Papír balí kámen! Prohrál jsi!.")
-                Pc += 1
-        elif user_action == "Papír" or "papír" or "Papir" or "papir":
-            if computer_action == "Kámen":
-                print("Papír balí kámen! Vyhrál jsi!")
-                Ty += 1
-            else:
-                print("Nužky stříhají papír! Prohrál jsi.")
-                Pc += 1
-        elif user_action == "Nužky" or "nužky" or "Nuzky" or "nuzky":
-            if computer_action == "Papír":
-                print("Nužky stříhají papír! Vyhrál jsi.!")
-                Ty += 1
-            else:
-                print("Kámen tupí nužky! Prohrál jsi.")
-                Pc += 1
+        if user_action in vyber:
+            print(
+                f"\nTvoje volba {user_action}, Volba počítače {computer_action}.\n")
+            if user_action == computer_action:
+                print(
+                    f"Oba jste vybrali stejný znak {user_action}. Je to remíza!")
+            elif user_action == "Kámen" or "kámen" or "Kamen" or "kamen":
+                if computer_action == "Nužky":
+                    print("Kámen tupí nužky! Vyhrál jsi.")
+                    Ty += 1
+                else:
+                    print("Papír balí kámen! Prohrál jsi!.")
+                    Pc += 1
+            elif user_action == "Papír" or "papír" or "Papir" or "papir":
+                if computer_action == "Kámen":
+                    print("Papír balí kámen! Vyhrál jsi!")
+                    Ty += 1
+                else:
+                    print("Nužky stříhají papír! Prohrál jsi.")
+                    Pc += 1
+            elif user_action == "Nužky" or "nužky" or "Nuzky" or "nuzky":
+                if computer_action == "Papír":
+                    print("Nužky stříhají papír! Vyhrál jsi.!")
+                    Ty += 1
+                else:
+                    print("Kámen tupí nužky! Prohrál jsi.")
+                    Pc += 1
         else:
             print("Neplatná hodnota")
             continue
